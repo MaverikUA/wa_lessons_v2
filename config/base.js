77aa57935f4ef91c713fcc02c66da7499cd36311
MaverikUA <terr0ristua@gmail.com>
2018-12-26 10:50:55 +0200

    config

---
+++ config/base.js
@@ -0,0 +1,117 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+const baseConf = (_path) => {
+    // NEW pages should be added here i.g. if you need to create
+    // contacts page you need add folder to src,
+    // and add path and name to entry array
+    const VENDORS_NAME = 'vendors';
+    const entry = {
+        index: ['babel-polyfill', './src/index/index.js'],
+        lesson_6: ['./src/lesson_6/lesson_6.js'],
+        lesson_6_test:['./src/lesson_6_test/lesson_6_test.js'],
+        homework_6:['./src/homework_6/homework_6.js'],
+        lesson_9:['./src/lesson_9/lesson_9.js'],
+        home_9:['./src/home_9/home_9.js'],
+        home_10:['./src/home_10/home_10.js'],
+        home_lights:['./src/home_lights/home_lights.js'],
+        lesson_11:['./src/lesson_11/lesson_11.js'],
+        home_11:['./src/home_11/home_11.js'],
+        home_12:['./src/home_12/home_12.js'],
+        lesson_13:['./src/lesson_13/lesson_13.js'],
+        home_13:['./src/home_13/home_13.js'],
+        lesson_14:['./src/lesson_14/lesson_14.js'],
+        home_14:['./src/home_14/home_14.js'],
+        
+    };
+
+    const plugins = Object.keys(entry).reduce((acc, name) => {
+        acc.push(new HtmlWebpackPlugin({
+            chunksSortMode: 'manual',
+            title: `${name}`,
+            template: `./src/${name}/${name}.html`,
+            chunks: [VENDORS_NAME, name],
+            filename: `./${name}.html`,
+        }));
+        acc.push(new ExtractTextPlugin({
+            filename: `[name].css`,
+            allChunks: false
+        }));
+
+        return acc;
+    }, []);
+
+    plugins.concat([
+        new webpack.optimize.CommonsChunkPlugin({
+            name: VENDORS_NAME,
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            VERSION: JSON.stringify('5fa3b9'),
+            BROWSER_SUPPORTS_HTML5: true,
+            'typeof window': JSON.stringify('object')
+        })
+    ]);
+
+    entry.vendors = `./src/common/scripts/${VENDORS_NAME}.js`;
+
+    return {
+        entry,
+        output: {
+            filename: '[name].js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: 'html-loader'
+                        }
+                    ]
+                },
+                {
+                    test: /\.js/,
+                    exclude: /(node_modules)/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['env']
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.scss/,
+                    loader: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'autoprefixer-loader?browsers=last 5 version', 'sass-loader']
+                    })
+                },
+                {
+
+                    /**
+                     * ASSET LOADER
+                     * Reference: https://github.com/webpack/file-loader
+                     * Copy png, jpg, jpeg, gif, svg, woff, woff2, ttf, eot files to output
+                     * Rename the file using the asset hash
+                     * Pass along the updated reference to your code
+                     * You can add here any file extension you want to get copied to your output
+                     */
+                    test: /\.(png|jpg|jpeg|gif|svg)$/,
+                    loader: 'file-loader?publicPath=./&name=assets/images/[name].[ext]'
+                },
+                {
+                    test: /\.(eot|ttf|woff|woff2)$/,
+                    loader: 'file-loader?publicPath=./&name=assets/fonts/[name].[ext]'
+                }
+            ]
+        },
+        plugins
+    };
+};
+
+module.exports = baseConf;
